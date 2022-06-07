6665c29c27e80d102c1ab23aa2880456a0ed7700
NikcQ <nikoo576@hotmail.com>
2022-06-06 23:24:18 -0500

    Created Beans, userService & userRepository

--- src/main/java/com/firma/interfaz/repositories/UserRepository.java
+++ src/main/java/com/firma/interfaz/repositories/UserRepository.java
@@ -3,12 +3,19 @@
  */
 package com.firma.interfaz.repositories;
 
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
-import com.firma.interfaz.entites.User;
+import io.spring.guides.gs_producing_web_service.User;
 
 /**
  * @author Marcos
@@ -16,5 +23,23 @@ import com.firma.interfaz.entites.User;
  */
 @Component
 public class UserRepository {
-	private static final Map<Integer, User> countries = new HashMap<>();
+	private static final Map<Integer, User> users = new HashMap<>();
+	
+	@PostConstruct
+	public void initData() {
+		User nicolas = new User();
+		nicolas.setFirstName("Nicolas");
+		nicolas.setLastName("La Rotta");
+		nicolas.setId(10);
+		nicolas.setEmail("testemail@somewhere.com");
+
+		users.put(nicolas.getId(), nicolas);
+
+	}
+
+	public User findUser(int id) {
+		Assert.notNull(id, "The country's name must not be null");
+		return users.get(id);
+	}
+	
 }
---
+++ src/main/java/com/firma/interfaz/services/UserService.java
@@ -0,0 +1,35 @@
+package com.firma.interfaz.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import com.firma.interfaz.repositories.*;
+
+import io.spring.guides.gs_producing_web_service.GetUserRequest;
+import io.spring.guides.gs_producing_web_service.GetUserResponse;
+
+@Endpoint
+public class UserService {
+
+	private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+
+	private UserRepository userRepository;
+
+	@Autowired
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+	@ResponsePayload
+	
+	public GetUserResponse getUser(@RequestPayload GetUserRequest request) {
+		GetUserResponse response = new GetUserResponse();
+		response.setUser(userRepository.findUser(request.getId()));
+
+		return response;
+	}
+	
+}
